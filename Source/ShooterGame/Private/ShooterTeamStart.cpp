6c68cb097fdcff86878279b8bda91b7bb521722f
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2018-03-26 13:57:10 +0200

    Initial commit of the ShooterGame C++ project for UE4.19

---
+++ Source/ShooterGame/Private/ShooterTeamStart.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "ShooterGame.h"
+#include "ShooterTeamStart.h"
+
+AShooterTeamStart::AShooterTeamStart(const FObjectInitializer& ObjectInitializer) 
+	: Super(ObjectInitializer) 
+{
+
+}
